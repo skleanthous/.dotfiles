73e1c861b6eb9c674000c8854d8660d6ed459e50
Savvas Kleanthous <skleanthous@users.noreply.github.com>
2020-08-16 00:43:27 +0100

    Install node, java, plantuml & hugo

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 sudo apt-get install git bash-completion
 
+#
+# Install node
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+sudo apt-get install hugo
+# install latest jdk
+apt-get install -y default-jdk
+apt-get install -y graphviz
+apt-get install -y plantuml
+
 while read EXTENSION
 do
     echo_info "Install VSCode Extension: $EXTENSION"
     code --install-extension $EXTENSION
 done < $HOME/.dotfiles/vscode/extensions.txt 
+
+
 
 ln -sf $HOME/.dotfiles/vscode/settings.json $HOME/.config/Code\ -\ OSS/User/settings.json
