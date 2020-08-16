7d7e7343f450ade67272777b941b91efd9a18e9a
Savvas Kleanthous <skleanthous@users.noreply.github.com>
2020-08-16 02:10:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo apt-get install hugo
 # install latest jdk
 sudo apt-get install -y default-jdk
 sudo apt-get install -y graphviz
-sudo hugoapt-get install -y plantuml
+sudo apt-get install -y plantuml
 
 while read EXTENSION
 do
