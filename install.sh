08bfd0072f4ffdab4588ea30f528108cc2ae21c7
Savvas Kleanthous <skleanthous@users.noreply.github.com>
2020-08-16 01:08:03 +0100

    Remove adding settings from install.sh

--- install.sh
+++ install.sh
@@ -11,13 +11,3 @@ sudo apt-get install hugo
 apt-get install -y default-jdk
 apt-get install -y graphviz
 apt-get install -y plantuml
-
-while read EXTENSION
-do
-    echo_info "Install VSCode Extension: $EXTENSION"
-    code --install-extension $EXTENSION
-done < $HOME/.dotfiles/vscode/extensions.txt 
-
-
-
-ln -sf $HOME/.dotfiles/vscode/settings.json $HOME/.config/Code\ -\ OSS/User/settings.json
