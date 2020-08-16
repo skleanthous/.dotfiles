074ebde4f8c2aeaa901e7920e9ac337cd41cfec1
Savvas Kleanthous <skleanthous@users.noreply.github.com>
2020-08-16 02:30:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y git bash-completion
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
-sudo apt-get install hugo
+sudo apt-get install -y hugo
 # install latest jdk
 sudo apt-get install -y default-jdk
 sudo apt-get install -y graphviz
